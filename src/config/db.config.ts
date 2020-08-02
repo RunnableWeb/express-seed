a669db431ebdcfa90ba52678a9da503557cf4199
samih.abuawad <samih.abuawad@gmail.com>
2020-08-02 06:22:03 +0300

    db config - fix connection when no username/password provided

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -4,12 +4,13 @@ export default async () => {
     const dbHost = process.env.DB_HOST || 'localhost';
     const dbPort = process.env.DB_PORT || 27017;
     const dbName = process.env.DB_NAME || 'rw-express-seed';
-    const dbUser = process.env.DB_USER || 'samih';
-    const dbPass = process.env.DB_PASS || '1234!';
+    const dbUser = process.env.DB_USER || undefined;
+    const dbPass = process.env.DB_PASS || undefined;
 
     try {
         mongoose.set('useCreateIndex', true);
-        await mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`, {
+        const userNameAndPassword = dbUser ? `${dbUser}:${dbPass}@` : '';
+        await mongoose.connect(`mongodb://${userNameAndPassword}@${dbHost}:${dbPort}/${dbName}`, {
             useNewUrlParser: true,
         });
         console.log(`console.log('Connected Successfully to DB')`);
